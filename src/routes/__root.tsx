88ccb6e86a5b6704792e1e0bfd2485e354d0e7e4
Tiago Cunha <tiago92cunha@gmail.com>
2025-02-22 16:15:25 +0000

    Init project

---
+++ src/routes/__root.tsx
@@ -0,0 +1,18 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import "../index.css";
+
+export const Route = createRootRoute({
+  component: () => (
+    <div className="h-screen bg-red-500">
+      <div className="p-2 flex gap-2 bg-red-500">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  ),
+});
